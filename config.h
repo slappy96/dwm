d854862d25f66603928ff20432caafe646608e4c
slappy96 <jqmunos@gmail.com>
2020-11-30 01:43:09 -0600

    added browser and nmtui to dwm bindings

--- config.h
+++ config.h
@@ -77,6 +77,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,             		XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,             		XK_v,      spawn,          SHCMD("virt-manager") },
+	{ MODKEY,             		XK_w,      spawn,          SHCMD("$BROWSER") },
+	{ MODKEY|ShiftMask,            	XK_w,      spawn,          SHCMD("st -e nmtui") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
