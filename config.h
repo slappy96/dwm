431c6e31f747ac22c167be6ce1976ca75b8ead7d
jason <jason@munos.xyz>
2021-05-01 17:55:36 -0500

    removed NL(no ligatures) from font

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono NL:size=11", "fontawesome:size=11" };
-static const char dmenufont[]       = "JetBrains Mono NL:size=11";
+static const char *fonts[]          = { "JetBrains Mono:size=12", "fontawesome:size=11" };
+static const char dmenufont[]       = "JetBrains Mono:size=11";
 /* background color */
 static const char col_black[]       = "#000000";
 /* inactive window border color */
