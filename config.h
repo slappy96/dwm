8e60756c6b06ac74f71af87c13699e8ab4401ee6
jason <jason@munos.xyz>
2021-05-31 14:50:47 -0500

    fixed rules for app start tags

--- config.h
+++ config.h
@@ -39,10 +39,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "LibreWolf",  NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Steam",    NULL,       NULL,       0,            0,           -1 },
-	{ "Lutris",   NULL,       NULL,       0,            0,           -1 },
+	{ "Gimp",     NULL,       NULL,       1 << 7,       1,           -1 },
+	{ "LibreWolf",NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Steam",    NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Lutris",   NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
