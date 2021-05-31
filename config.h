422eae4dfc73b7906a9f5c3743526aa0cd9c98fa
jason <jason@munos.xyz>
2021-05-31 11:29:43 -0500

    removed floating behavior for steam/lutris

--- config.h
+++ config.h
@@ -41,8 +41,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Steam",    NULL,       NULL,       0,            1,           -1 },
-	{ "Lutris",   NULL,       NULL,       0,            1,           -1 },
+	{ "Steam",    NULL,       NULL,       0,            0,           -1 },
+	{ "Lutris",   NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
