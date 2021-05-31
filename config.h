a5c1eab8d6287e78a5d11eb08e4e73acab20cacb
jason <jason@munos.xyz>
2021-05-31 15:03:27 -0500

    tweaking static const rules[]

--- config.h
+++ config.h
@@ -38,11 +38,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 7,       1,           -1 },
-	{ "LibreWolf",NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Steam",    NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Lutris",   NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class           instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",          NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "LibreWolf",     NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Steam",         NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Lutris",        NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Virt-manager",  NULL,       NULL,       1 << 6,       0,           -1 },
 };
 
 /* layout(s) */
