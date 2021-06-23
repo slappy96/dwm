f0c7f71f2cdae77885efa32150092307e5ae5a54
jason <jason@munos.xyz>
2021-06-22 21:45:32 -0500

    changed default browser to brave

--- config.h
+++ config.h
@@ -36,7 +36,7 @@ static const Rule rules[] = {
 	 */
 	/* class           instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",          NULL,       NULL,       1 << 7,       0,           -1 },
-	{ "LibreWolf",     NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Brave-browser", NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "Steam",         NULL,       NULL,       1 << 8,       1,           -1 },
 	{ "Lutris",        NULL,       NULL,       1 << 8,       1,           -1 },
 	{ "Virt-manager",  NULL,       NULL,       1 << 6,       0,           -1 },
